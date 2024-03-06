fbfdae6e2323db6a4cf8911272a0eaa11f1e0308
victoriatr6n <108353792+victoriatr6n@users.noreply.github.com>
2024-03-06 14:08:11 -0800

    lab6
    changes to bash and tests

--- grade.sh
+++ grade.sh
@@ -8,6 +8,46 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "file found"
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+else
+    echo "Missing student-submission/ListExamples.java, did you forget the file? check if you named it wrong"
+    exit 1 
+fi
+
+pwd
+cd grading-area
+pwd
+
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
+javac -cp $CPATH *.java 
+
+if [[ $? -ne 0 ]]
+then
+  echo "The program failed to compile, see compile error above"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+firstword=$(echo "$lastline" | awk '{print $1}')
+if [ "$firstword" == "OK" ] 
+then
+    echo "2/2 passed"
+    exit
+fi
+
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((expr $tests - $failures)) 
+
+
+echo "Your score is $successes / $tests"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
